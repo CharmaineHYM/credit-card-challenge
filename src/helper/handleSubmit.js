7c98e21e2deac9fe48e7a0c8e0b762b1130ef81f
Charmaine <charmaine.hym@gmail.com>
2022-08-13 18:34:07 +0800

    simplify code

--- src/helper/handleSubmit.js
+++ src/helper/handleSubmit.js
@@ -2,94 +2,87 @@ import { store } from '../store/Store'
 
 let error = {
     typeOne: 'Wong format, please check again',
-    typeTwo: "Can't be blank"
+    typeTwo: "Can't be blank",
+    typeThree: "Only accept Visa, Master or AE"
 }
 
 function validateCardName(value){
-    if(!value){
-        store.value.cardNameErr = error.typeTwo
-    }
-    else{
-        store.value.cardNameErr = ''
-        return true
-    }
-
-    return false
+    value = value ? value.toString() : "";
+    let err = ''
+    let isFilled = value.length
+    if(!isFilled){ err = error.typeTwo }
+    store.value.cardNameErr = err
+    let isValid = isFilled
+    return isValid
 }
 
 function validateCardNumber(value){
-    if(!value){
-        store.value.cardNbErr = error.typeTwo
-    }
-    else{
-        if(!(value.startsWith(1) || value.startsWith(4) || value.startsWith(5))){
-            store.value.cardNbErr = error.typeOne
-        }else{
-            if(!(value.length === 16)){
-                store.value.cardNbErr = error.typeOne
-            }
-            else{
-                store.value.cardNbErr = ''
-                return true
-            }
-        }
-        
-    }
+    value = value ? value.toString() : "";
+    let err = ''
+    let isFilled = value.length
+    let isLongEnough = value.length === 16
+    let isValidCard = ["1","4","5"].includes( value[0] );
 
-    return false
+    if( !isFilled ) { err = error.typeTwo }
+    else if( !isLongEnough ) { err = error.typeOne }
+    else if( !isValidCard ) { err = error.typeThree }
+    store.value.cardNbErr = err;
+    let isValid = isFilled && isLongEnough && isValidCard;
+    return isValid; 
 }
 
 function validateExpDate(month, year){
+    month = month ? month.toString() : "";
+    year = year ? year.toString() : "";
+    let err = ''
     let today = new Date();
     let thisYear = today.getFullYear().toString().slice(2)
     let thisMonth = today.getMonth() +1
 
-    if(!month || !year){
-        store.value.expDateErr = error.typeTwo
-    }
-    else{
-        if(parseInt(month) > 12 || 
-        parseInt(year) < parseInt(thisYear) ||
-        (parseInt(year) == parseInt(thisYear) && 
-        parseInt(month) < thisMonth)){
-            store.value.expDateErr = error.typeOne
-        }
-        else{
-            store.value.expDateErr = ''
-            return true
-        }
-    }
-    return false
+    let isFilled = month.length && year.length
+    let isDateValid = 
+    parseInt(month) < 12 || 
+    parseInt(year) > parseInt(thisYear) ||
+    (parseInt(year) == parseInt(thisYear) && 
+    parseInt(month) > thisMonth)
+
+    if( !isDateValid ) { err = error.typeOne }
+    if( !isFilled ) { err = error.typeTwo }
+
+    store.value.expDateErr = err
+
+    let isValid = isFilled && isDateValid
+
+    return isValid
 }
 
 
 function validateCvc(value){
-    if(!value){
-        store.value.cvcErr = error.typeTwo
-    }else{
-        if(value.length < 3){
-            store.value.cvcErr = error.typeOne
-        }
-        else{
-            store.value.cvcErr = ''
-            return true
-        }
-    }
-    return false
+    value = value ? value.toString() : "";
+    let err = ''
+    let isFilled = value.length
+    let isLongEnough = value.length >= 3
+
+    if( !isFilled ) { err = error.typeTwo }
+    else if( !isLongEnough ) { err = error.typeOne }
+    store.value.cvcErr = err
+    let isValid = isFilled && isLongEnough
+
+    return isValid
 }
 
-export function handleSubmit(){
-    let isValidCardName = validateCardName(store.value.cardName)
-    let isValidCardNb = validateCardNumber(store.value.cardNb)
-    let isValidExpDate = validateExpDate(store.value.expMonth, store.value.expYear)
-    let isValidCvc = validateCvc(store.value.cvc)
-    if(
-        isValidCardName &&
+
+export function handleSubmit() {
+    let { cardName, cardNb, expMonth, expYear, cvc } = store.value;
+    let isValidCardName = validateCardName(cardName) 
+    let isValidCardNb = validateCardNumber(cardNb) 
+    let isValidExpDate = validateExpDate(expMonth, expYear) 
+    let isValidCvc = validateCvc(cvc) 
+
+    store.value.isValid = (
+        isValidCardName && 
         isValidCardNb && 
         isValidExpDate && 
         isValidCvc
-    ){
-       store.value.isValid = true
-    }
-    
+    );
 }
